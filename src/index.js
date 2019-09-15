008e50ce96249eec3cb90b6abeb5d3ebcd0a5482
Kevin Gonzalez <typekev@gmail.com>
2019-09-15 23:38:11 +0200

    Add more trusted origins

--- src/index.js
+++ src/index.js
@@ -102,7 +102,11 @@ server.post('/api/messages', (req, res) => {
 
 function trustedOrigin(origin) {
   return (
-    /^https?:\/\/localhost([\/:]|$)/.test(origin) || /^https?:\/\/typekev([\/:]|$)/.test(origin)
+    /^https?:\/\/localhost([\/:]|$)/.test(origin) ||
+    /^https?:\/\/webchat([\/:]|$)/.test(origin) ||
+    /^https?:\/\/[\d\w]+\.ngrok\.io([\/:]|$)/.test(origin) ||
+    /^https:\/\/typekev-bot\.azurewebsites\.net/.test(origin) ||
+    /^https:\/\/typekev\.com/.test(origin)
   );
 }
 
