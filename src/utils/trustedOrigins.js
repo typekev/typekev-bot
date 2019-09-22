a3d89f65ba808334c90bad05265fd2726fae4591
Kevin Gonzalez <typekev@gmail.com>
2019-09-22 22:17:04 +0200

    Add netlify to trustedOrigin

--- src/utils/trustedOrigins.js
+++ src/utils/trustedOrigins.js
@@ -3,6 +3,7 @@ const trustedOrigin = origin =>
   /^https?:\/\/webchat([/:]|$)/.test(origin) ||
   /^https?:\/\/[\d\w]+\.ngrok\.io([/:]|$)/.test(origin) ||
   /^https:\/\/typekev-bot\.azurewebsites\.net/.test(origin) ||
+  /^https:\/\/typekev\.netlify\.com/.test(origin) ||
   /^https:\/\/typekev\.com/.test(origin);
 
 export default trustedOrigin;
