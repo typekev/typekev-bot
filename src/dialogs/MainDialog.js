bc9e6c49946a0b7efa121ce1cb931d1a02297fd0
Kevin Gonzalez <typekev@gmail.com>
2019-09-22 20:52:41 +0200

    Simplify dialog flow

--- src/dialogs/MainDialog.js
+++ src/dialogs/MainDialog.js
@@ -1,4 +1,4 @@
-import { MessageFactory, InputHints } from 'botbuilder';
+import { InputHints } from 'botbuilder';
 import { LuisRecognizer } from 'botbuilder-ai';
 import {
   ComponentDialog,
@@ -9,6 +9,13 @@ import {
 } from 'botbuilder-dialogs';
 
 const MAIN_WATERFALL_DIALOG = 'mainWaterfallDialog';
+
+const finalStep = async stepContext => {
+  const messageText = 'Can I help you with something else?';
+  await stepContext.context.sendActivity(messageText, null, InputHints.IgnoringInput);
+  const end = await stepContext.endDialog();
+  return end;
+};
 
 export default class MainDialog extends ComponentDialog {
   constructor(luisRecognizer, qnaMaker) {
@@ -23,11 +30,7 @@ export default class MainDialog extends ComponentDialog {
     // Define the main dialog and its related components.
     // This is a sample "book a flight" dialog.
     this.addDialog(new TextPrompt('TextPrompt')).addDialog(
-      new WaterfallDialog(MAIN_WATERFALL_DIALOG, [
-        this.introStep.bind(this),
-        this.actStep.bind(this),
-        this.finalStep.bind(this),
-      ]),
+      new WaterfallDialog(MAIN_WATERFALL_DIALOG, [this.introStep.bind(this), finalStep]),
     );
 
     this.initialDialogId = MAIN_WATERFALL_DIALOG;
@@ -69,20 +72,6 @@ export default class MainDialog extends ComponentDialog {
       return next;
     }
 
-    const messageText = stepContext.options.restartMsg ? stepContext.options.restartMsg : 'restart';
-    const promptMessage = MessageFactory.text(messageText, messageText, InputHints.ExpectingInput);
-    const prompt = await stepContext.prompt('TextPrompt', { prompt: promptMessage });
-    return prompt;
-  }
-
-  /**
-   * Second step in the waterfall.
-   * This will use LUIS to attempt to extract the origin, destination and travel dates.
-   * Then, it hands off to the bookingDialog child dialog to collect any remaining details.
-   */
-  async actStep(stepContext) {
-    // Call LUIS and gather any potential booking details.
-    // (Note the TurnContext has the response to the prompt)
     const luisResult = await this.luisRecognizer.executeLuisQuery(stepContext.context);
     switch (LuisRecognizer.topIntent(luisResult)) {
       default:
@@ -91,18 +80,5 @@ export default class MainDialog extends ComponentDialog {
 
     const next = await stepContext.next();
     return next;
-  }
-
-  async finalStep(stepContext) {
-    if (stepContext.result) {
-      const msg = `final step`;
-      await stepContext.context.sendActivity(msg, msg, InputHints.IgnoringInput);
-    }
-
-    // Restart the main dialog with a different message the second time around
-    const replaceDialog = await stepContext.replaceDialog(this.initialDialogId, {
-      restartMsg: 'What else can I do for you?',
-    });
-    return replaceDialog;
   }
 }
