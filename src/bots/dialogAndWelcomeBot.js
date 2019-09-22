8136a9df15ce841796a48ccbae69fb28bd4e1ea4
Kevin Gonzalez <typekev@gmail.com>
2019-09-22 18:06:27 +0200

    Drop welcome card

--- src/bots/dialogAndWelcomeBot.js
+++ src/bots/dialogAndWelcomeBot.js
@@ -1,29 +1,14 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-import { CardFactory } from 'botbuilder';
-
-import { DialogBot } from './dialogBot';
-import WelcomeCard from './resources/welcomeCard.json';
+import DialogBot from './dialogBot';
 
 class DialogAndWelcomeBot extends DialogBot {
   constructor(conversationState, userState, dialog) {
     super(conversationState, userState, dialog);
 
-    this.onMembersAdded(async (context, next) => {
-      const { membersAdded } = context.activity;
-      for (let cnt = 0; cnt < membersAdded.length; cnt++) {
-        if (membersAdded[cnt].id !== context.activity.recipient.id) {
-          const welcomeCard = CardFactory.adaptiveCard(WelcomeCard);
-          await context.sendActivity({ attachments: [welcomeCard] });
-          await dialog.run(context, conversationState.createProperty('DialogState'));
-        }
-      }
-
+    this.onMembersAdded(async (_context, next) => {
       // By calling next() you ensure that the next BotHandler is run.
       await next();
     });
   }
 }
 
-export { DialogAndWelcomeBot };
+export default DialogAndWelcomeBot;
