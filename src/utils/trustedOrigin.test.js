c5ea055c7d423c8ebf41564a83df13f7f9fb9b1e
Kevin Gonzalez <typekev@gmail.com>
2020-02-17 23:21:06 +0100

    Add tests for typekev.netlify.com

--- src/utils/trustedOrigin.test.js
+++ src/utils/trustedOrigin.test.js
@@ -5,6 +5,10 @@ describe('Tests trusted origins', () => {
     expect(trustedOrigin('https://typekev.com')).toBe(true);
   });
 
+  it('tests typekev.netlify.com', () => {
+    expect(trustedOrigin('https://typekev.netlify.com')).toBe(true);
+  });
+
   it('tests typekev.netlify.app', () => {
     expect(trustedOrigin('https://typekev.netlify.app')).toBe(true);
   });
