1f88b1ced4df580da5eb6f61301a56fc65e4e440
Kevin Gonzalez <typekev@gmail.com>
2020-02-19 23:21:31 +0100

    Add tests for local

--- src/utils/trustedOrigin.js
+++ src/utils/trustedOrigin.js
@@ -1,6 +1,5 @@
 const trustedOrigin = origin =>
   /^https?:\/\/localhost([/:]|$)/.test(origin) ||
-  /^https?:\/\/webchat([/:]|$)/.test(origin) ||
   /^https?:\/\/[\d\w]+\.ngrok\.io([/:]|$)/.test(origin) ||
   /^https:\/\/typekev-bot\.azurewebsites\.net/.test(origin) ||
   /^https:\/\/typekev\.netlify\.com/.test(origin) ||
--- src/utils/trustedOrigin.test.js
+++ src/utils/trustedOrigin.test.js
@@ -1,6 +1,16 @@
 import trustedOrigin from './trustedOrigin';
 
 describe('Tests trusted origins', () => {
+  it('tests localhost', () => {
+    expect(trustedOrigin('http://localhost')).toBe(true);
+    expect(trustedOrigin('https://localhost')).toBe(true);
+  });
+
+  it('tests ngrok.io', () => {
+    expect(trustedOrigin('http://test123.ngrok.io')).toBe(true);
+    expect(trustedOrigin('https://test123.ngrok.io')).toBe(true);
+  });
+
   it('tests typekev.com', () => {
     expect(trustedOrigin('https://typekev.com')).toBe(true);
   });
@@ -15,9 +25,5 @@ describe('Tests trusted origins', () => {
 
   it('tests typekev.netlify.app', () => {
     expect(trustedOrigin('https://typekev.netlify.app')).toBe(true);
-  });
-
-  it('tests localhost', () => {
-    expect(trustedOrigin('http://localhost')).toBe(true);
   });
 });
