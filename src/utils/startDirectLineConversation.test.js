f54708e3d52811cab807e0ab64b4c8d9167e1495
Kevin Gonzalez <typekev@gmail.com>
2020-02-20 23:52:40 +0100

    Add test for fetch args

--- src/utils/startDirectLineConversation.test.js
+++ src/utils/startDirectLineConversation.test.js
@@ -1,5 +1,10 @@
 import startDirectLineConversation from './startDirectLineConversation';
 
+global.process.env = Object.assign(process.env, {
+  DIRECT_LINE_URL: 'https://directline.botframework.com/',
+  DIRECT_LINE_SECRET: 'DIRECT_LINE_SECRET',
+});
+
 describe('Tests starting a conversation', () => {
   it('tests fetch response', () => {
     const mockFetchPromise = Promise.resolve({
@@ -9,7 +14,12 @@ describe('Tests starting a conversation', () => {
 
     global.fetch = jest.fn().mockImplementation(() => mockFetchPromise);
 
+    const { DIRECT_LINE_URL, DIRECT_LINE_SECRET } = process.env;
     startDirectLineConversation();
     expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith(`${DIRECT_LINE_URL}v3/directline/conversations`, {
+      headers: { authorization: `Bearer ${DIRECT_LINE_SECRET}` },
+      method: 'POST',
+    });
   });
 });
