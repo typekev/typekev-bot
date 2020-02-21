12eca2ac25f1c660df84e273b5f77b4a6a14c905
Kevin Gonzalez <typekev@gmail.com>
2020-02-21 23:59:08 +0100

    Add test for 500

--- src/utils/startDirectLineConversation.test.js
+++ src/utils/startDirectLineConversation.test.js
@@ -5,21 +5,20 @@ global.process.env = Object.assign(process.env, {
   DIRECT_LINE_SECRET: 'DIRECT_LINE_SECRET',
 });
 
-const mockFetchPromise = Promise.resolve({
-  status: 200,
-  json: () => Promise.resolve({ status: 200 }),
-});
-
-global.fetch = jest.fn().mockImplementation(() => mockFetchPromise);
-
 const { DIRECT_LINE_URL, DIRECT_LINE_SECRET } = process.env;
 
+const mockFetchPromise = (status = 200, rest) =>
+  Promise.resolve({
+    status,
+    json: () => Promise.resolve({ status, ...rest }),
+  });
 describe('Tests starting a conversation', () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
   it('tests startDirectLineConversation with successful response', async () => {
+    global.fetch = jest.fn().mockImplementation(() => mockFetchPromise(200));
     const response = await startDirectLineConversation();
     expect(global.fetch).toHaveBeenCalledTimes(1);
     expect(global.fetch).toHaveBeenCalledWith(`${DIRECT_LINE_URL}v3/directline/conversations`, {
@@ -29,9 +28,32 @@ describe('Tests starting a conversation', () => {
     expect(response.status).toBe(200);
   });
   it('tests startDirectLineConversation with error response', async () => {
+    global.fetch = jest
+      .fn()
+      .mockImplementation(() => mockFetchPromise(200, { error: 'test error' }));
+
     try {
       expect(await startDirectLineConversation()).toThrow();
-    } catch (_e) {
+    } catch (error) {
+      expect(error).toEqual(
+        new Error('Direct Line service responded "test error" while generating new token'),
+      );
+      expect(global.fetch).toHaveBeenCalledTimes(1);
+      expect(global.fetch).toHaveBeenCalledWith(`${DIRECT_LINE_URL}v3/directline/conversations`, {
+        headers: { authorization: `Bearer ${DIRECT_LINE_SECRET}` },
+        method: 'POST',
+      });
+    }
+  });
+
+  it('tests startDirectLineConversation with error response', async () => {
+    global.fetch = jest.fn().mockImplementation(() => mockFetchPromise(500));
+    try {
+      expect(await startDirectLineConversation()).toThrow();
+    } catch (error) {
+      expect(error).toEqual(
+        new Error('Direct Line service returned 500 while generating new token'),
+      );
       expect(global.fetch).toHaveBeenCalledTimes(1);
       expect(global.fetch).toHaveBeenCalledWith(`${DIRECT_LINE_URL}v3/directline/conversations`, {
         headers: { authorization: `Bearer ${DIRECT_LINE_SECRET}` },
