325f7aa63516e75cfd3ee711dca018c346efec4a
Kevin Gonzalez <typekev@gmail.com>
2020-02-21 23:43:50 +0100

    Add test for startDirectLineConversation with error response

--- src/utils/startDirectLineConversation.test.js
+++ src/utils/startDirectLineConversation.test.js
@@ -5,16 +5,21 @@ global.process.env = Object.assign(process.env, {
   DIRECT_LINE_SECRET: 'DIRECT_LINE_SECRET',
 });
 
+const mockFetchPromise = Promise.resolve({
+  status: 200,
+  json: () => Promise.resolve({ status: 200 }),
+});
+
+global.fetch = jest.fn().mockImplementation(() => mockFetchPromise);
+
+const { DIRECT_LINE_URL, DIRECT_LINE_SECRET } = process.env;
+
 describe('Tests starting a conversation', () => {
-  it('tests fetch success response', async () => {
-    const mockFetchPromise = Promise.resolve({
-      status: 200,
-      json: () => Promise.resolve({ status: 200 }),
-    });
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-    global.fetch = jest.fn().mockImplementation(() => mockFetchPromise);
-
-    const { DIRECT_LINE_URL, DIRECT_LINE_SECRET } = process.env;
+  it('tests startDirectLineConversation with successful response', async () => {
     const response = await startDirectLineConversation();
     expect(global.fetch).toHaveBeenCalledTimes(1);
     expect(global.fetch).toHaveBeenCalledWith(`${DIRECT_LINE_URL}v3/directline/conversations`, {
@@ -22,5 +27,16 @@ describe('Tests starting a conversation', () => {
       method: 'POST',
     });
     expect(response.status).toBe(200);
+  });
+  it('tests startDirectLineConversation with error response', async () => {
+    try {
+      expect(await startDirectLineConversation()).toThrow();
+    } catch (_e) {
+      expect(global.fetch).toHaveBeenCalledTimes(1);
+      expect(global.fetch).toHaveBeenCalledWith(`${DIRECT_LINE_URL}v3/directline/conversations`, {
+        headers: { authorization: `Bearer ${DIRECT_LINE_SECRET}` },
+        method: 'POST',
+      });
+    }
   });
 });
