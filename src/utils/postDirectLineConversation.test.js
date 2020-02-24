1569a3ba7d44d41e16a02a77beca242db1581b7c
Kevin Gonzalez <typekev@gmail.com>
2020-02-24 23:50:37 +0100

    test postDirectLineConversation with throw

--- src/utils/postDirectLineConversation.test.js
+++ src/utils/postDirectLineConversation.test.js
@@ -57,4 +57,27 @@ describe('Tests starting a conversation', () => {
     expect(send).toHaveBeenCalledWith(403, 'not trusted origin');
     expect(response).toBe(undefined);
   });
+
+  it('tests postDirectLineConversation with throw', async () => {
+    global.fetch = jest.fn().mockImplementation(() => ({ status: 200 }));
+    const jsonObj = JSON.stringify({ token: 'token' });
+    renewDirectLineToken.default = jest.fn().mockImplementation(() => {
+      throw new Error('test error');
+    });
+
+    const origin = 'https://typekev.com';
+    const header = jest.fn().mockImplementation(param => param === 'origin' && origin);
+    const req = { header, body: jsonObj };
+    const send = jest.fn();
+    const res = { send };
+    const response = await postDirectLineConversation(req, res);
+    expect(header).toHaveBeenCalledTimes(1);
+    expect(header).toHaveBeenCalledWith('origin');
+    expect(send).toHaveBeenCalledTimes(1);
+    expect(renewDirectLineToken.default).toHaveBeenCalledTimes(1);
+    expect(send).toHaveBeenCalledWith(500, 'test error', {
+      'Access-Control-Allow-Origin': '*',
+    });
+    expect(response).toBe(origin);
+  });
 });
