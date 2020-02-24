0b6f72032b46a3a8345a5ce9ec61ef9180020d05
Kevin Gonzalez <typekev@gmail.com>
2020-02-24 23:45:19 +0100

    Add test for postDirectLineConversation with token

--- src/utils/postDirectLineConversation.test.js
+++ src/utils/postDirectLineConversation.test.js
@@ -1,6 +1,28 @@
 import postDirectLineConversation from './postDirectLineConversation';
+import * as startDirectLineConversation from './startDirectLineConversation';
 
 describe('Tests starting a conversation', () => {
+  it('tests postDirectLineConversation without token', async () => {
+    global.fetch = jest.fn().mockImplementation(() => ({ status: 200 }));
+    const jsonObj = JSON.stringify({});
+    startDirectLineConversation.default = jest.fn().mockImplementation(() => jsonObj);
+
+    const origin = 'https://typekev.com';
+    const header = jest.fn().mockImplementation(param => param === 'origin' && origin);
+    const req = { header, body: jsonObj };
+    const send = jest.fn();
+    const res = { send };
+    const response = await postDirectLineConversation(req, res);
+    expect(header).toHaveBeenCalledTimes(1);
+    expect(header).toHaveBeenCalledWith('origin');
+    expect(send).toHaveBeenCalledTimes(1);
+    expect(startDirectLineConversation.default).toHaveBeenCalledTimes(1);
+    expect(send).toHaveBeenCalledWith(jsonObj, {
+      'Access-Control-Allow-Origin': '*',
+    });
+    expect(response).toBe(origin);
+  });
+
   it('tests postDirectLineConversation with a bad origin', async () => {
     const header = jest.fn().mockImplementation(param => param === 'origin' && 'http://google.com');
     const req = { header };
