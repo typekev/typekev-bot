355c8109f9c21420f99ccd590c6c32d1d31ac955
Kevin Gonzalez <typekev@gmail.com>
2020-02-26 23:35:39 +0100

    Add test for directline token renewal response with error in json

--- src/utils/renewDirectLineToken.test.js
+++ src/utils/renewDirectLineToken.test.js
@@ -25,4 +25,34 @@ describe('Tests renewing a directline token', () => {
       );
     }
   });
+
+  it('tests a directline token renewal response with error in json', async () => {
+    const errorMessage = 'test error';
+    const json = jest.fn().mockImplementation(() => ({ error: errorMessage }));
+    const response = { status: 200, json };
+    global.fetch = jest.fn().mockImplementation(() => response);
+    const token = 'test token';
+
+    try {
+      expect(await renewDirectLineToken(token)).toThrow();
+    } catch (error) {
+      expect(error).toEqual(
+        new Error(
+          `Direct Line service responded ${JSON.stringify(errorMessage)} while renewing token`,
+        ),
+      );
+      expect(global.fetch).toHaveBeenCalledTimes(1);
+      expect(json).toHaveBeenCalledTimes(1);
+      expect(global.fetch).toHaveBeenCalledWith(
+        'https://directline.botframework.com/v3/directline/tokens/refresh',
+        {
+          headers: {
+            authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+          method: 'POST',
+        },
+      );
+    }
+  });
 });
