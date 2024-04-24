0785a11b7bb4a63043a952d86e416015354e40f9
Kevin Gonzalez <kevin.gonzalez@ses.com>
2024-04-25 01:52:10 +0200

    Adds inner text search to getChatSuggestion

--- src/bots/helpers/getChatSuggestion.ts
+++ src/bots/helpers/getChatSuggestion.ts
@@ -3,8 +3,35 @@ interface GetChatSuggestions {
 }
 
 export function getChatSuggestion(this: GetChatSuggestions, text: string) {
-  return this.suggestions.find(
-    suggestion =>
-      suggestion.toLowerCase().substring(0, text.length) === text.toLowerCase(),
-  );
+  const lowerTexts = text.toLowerCase().split(' ');
+
+  return this.suggestions.find(suggestion => {
+    const lowerSuggestionWords = suggestion.toLowerCase().split(' ');
+    let suggestionIndex = 0;
+    let textIndex = 0;
+
+    while (
+      suggestionIndex < lowerSuggestionWords.length &&
+      textIndex < lowerTexts.length
+    ) {
+      if (
+        lowerSuggestionWords[suggestionIndex].startsWith(lowerTexts[textIndex])
+      ) {
+        if (
+          suggestionIndex < lowerSuggestionWords.length - 1 &&
+          textIndex < lowerTexts.length - 1
+        ) {
+          const nextSuggestionWord = lowerSuggestionWords[suggestionIndex + 1];
+          const nextTextWord = lowerTexts[textIndex + 1];
+          if (!nextSuggestionWord.startsWith(nextTextWord)) {
+            return false;
+          }
+        }
+        textIndex++;
+      }
+      suggestionIndex++;
+    }
+
+    return textIndex === lowerTexts.length;
+  });
 }
